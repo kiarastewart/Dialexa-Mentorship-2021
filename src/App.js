991be3090d1b0eb14f9ce2241f943247ed40e948
kiarastewart <85205932+kiarastewart@users.noreply.github.com>
2021-06-02 11:57:33 -0500

    Added Gradient Buttons

--- src/App.js
+++ src/App.js
@@ -39,6 +39,20 @@ const TestButton = withStyles({
   }
 })(Button);
 
+const GradientButton = withStyles({
+  root: {
+    background: "linear-gradient(45deg, #fd1d1d, #fcb045)",
+    textTransform: "none",
+    borderRadius: "36px",
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 36px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    fontFamily: "Poppins"
+  }
+})(Button);
+
 const DisabledButton = withStyles({
   fontSize: 16,
   fontFamily: "Poppins"
@@ -55,6 +69,7 @@ export default function CustomizedButtons() {
 
   return (
     <div>
+      {/* Customized Buttons overriding root */}
       <PleaseButton variant="contained" className={classes.margin}>
         Please Work
       </PleaseButton>
@@ -62,6 +77,14 @@ export default function CustomizedButtons() {
       <TestButton variant="outlined" size="small" className={classes.margin}>
         Test
       </TestButton>
+
+      <GradientButton
+        variant="outlined"
+        size="small"
+        className={classes.margin}
+      >
+        Gradient
+      </GradientButton>
 
       <DisabledButton
         variant="contained"
@@ -72,11 +95,35 @@ export default function CustomizedButtons() {
         Disabled
       </DisabledButton>
 
-      <Button size="small">Default</Button>
-      <Button size="medium" color="primary">
+      {/* Text Buttons*/}
+      <Button size="small" className={classes.margin}>
+        Default
+      </Button>
+      <Button size="medium" color="primary" className={classes.margin}>
         Primary
       </Button>
-      <Button size="large" color="secondary">
+      <Button size="large" color="secondary" className={classes.margin}>
+        Secondary
+      </Button>
+
+      {/* Outlined Buttons*/}
+      <Button variant="outlined" className={classes.margin}>
+        Default
+      </Button>
+      <Button
+        variant="outlined"
+        size="small"
+        color="primary"
+        className={classes.margin}
+      >
+        Primary
+      </Button>
+      <Button
+        variant="outlined"
+        size="large"
+        color="secondary"
+        className={classes.margin}
+      >
         Secondary
       </Button>
     </div>
