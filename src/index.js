a1abf1ac3648c4a886071c621349d36a71951724
kiarastewart <85205932+kiarastewart@users.noreply.github.com>
2021-06-02 19:46:11 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme"; //trying to import theme into index
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   rootElement
 );
